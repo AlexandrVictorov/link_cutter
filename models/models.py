d905abf0a6e9660e51215d2dd3b4bf6d4ab50520
Alex <alex@MacBook-Pro-Alex.local>
2026-03-07 17:55:32 +0300

    From lection

---
+++ models/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, Boolean
+from datetime import datetime
+
+metadata = MetaData()
+
+role = Table(
+    "role",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("permissions", JSON),
+)
+
+user = Table(
+    "user",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("email", String, nullable=False),
+    Column("username", String, nullable=False),
+    Column("hashed_password", String, nullable=False),
+    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
+    Column("role_id", Integer, ForeignKey('role.id')),
+)
